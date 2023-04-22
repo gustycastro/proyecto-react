436e880da07c35d80fb9041f8472ff99923448b6
gustycastro <gusty12rd@gmail.com>
2023-04-22 12:32:02 -0300

    PreEntrega2 Castro

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ export const NavBar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
-                <div className="collapse navbar-collapse">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <Categorias />
                 </div>
                 <CartWidget cantCarrito={0} />
